2a2df1f6ba82933b1b16c31a426a4ca394230ffb
asherdrake <asherdrake.alacar@sjsu.edu>
2022-10-13 19:19:59 -0700

    Mute Function
    Pressing key "G" mutes the AudioListener, and pressing "G" again unmutes it.

---
+++ No Evil/Assets/Scripts/toggleAudioListener.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class toggleAudioListener : MonoBehaviour
+{
+    public bool muted;
+    
+    void Start()
+    {
+        //ears = GetComponent<AudioListener>();
+        print(AudioListener.volume);
+        muted = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("g"))
+        {
+            if (!muted)
+            {
+                muted = !muted;
+                AudioListener.volume = 0;
+                print("Volume set to " + AudioListener.volume);
+
+            }
+            else
+            {
+                muted = !muted;
+                AudioListener.volume = 1;
+                print("Volume set to " + AudioListener.volume);
+            }
+        }
+        
+       /* if (Input.GetKeyDown("g") && muted)
+        {
+            AudioListener.volume = 1;
+            muted = false;
+            print("Volume set to " + AudioListener.volume);
+        }
+       */
+    }
+}
